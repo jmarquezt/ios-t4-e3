c6f5b94be7b0d5fe81eb0e7000f0421540e85333
Jorge Marquez Torres <jmarquez@jorges-mac.local>
2016-01-28 13:43:40 +0100

    Ejercicio 3 del Tema 4 completo

--- T4E3/GameScene.swift
+++ T4E3/GameScene.swift
@@ -11,18 +11,34 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        /*let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
+        self.addChild(myLabel)*/
+        /* Definimos el Menú Principal */
+        self.backgroundColor = UIColor(red: 0.15, green: 0.15, blue:
+            0.3, alpha: 1.0)
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Videojuego con SpriteKit";
+        myLabel.fontSize = 20;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame),
+            y:CGRectGetMidY(self.frame));
         self.addChild(myLabel)
+        
+        let myLabel2 = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel2.text = "Toca para Empezar";
+        myLabel2.fontSize = 15;
+        myLabel2.position = CGPoint(x:CGRectGetMidX(self.frame),
+            y:CGRectGetMidY(self.frame) - 100);
+        self.addChild(myLabel2)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
+        /*for touch in touches {
             let location = touch.locationInNode(self)
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
@@ -36,7 +52,11 @@ class GameScene: SKScene {
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
-        }
+        }*/
+        //Al tocar la pantalla cambiamos la escena
+        let transicion = SKTransition.doorsOpenVerticalWithDuration(1.0)
+        let juego = Videojuego(size: self.size)
+        self.scene?.view?.presentScene(juego, transition: transicion)
     }
    
     override func update(currentTime: CFTimeInterval) {
